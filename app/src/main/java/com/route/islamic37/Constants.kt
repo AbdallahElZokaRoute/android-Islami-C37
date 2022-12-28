c041aea6ff9e14902aa582b22d37ac797a786f1a
Shady Gamal <Shady_ql@yahoo.com>
2022-12-28 13:54:41 +0200

    Added Tasbeh feature.

---
+++ app/src/main/java/com/route/islamic37/Constants.kt
@@ -0,0 +1,12 @@
+package com.route.islamic37
+
+class Constants {
+
+    companion object {
+
+        public val subhan_allah: String = "سبحان الله"
+        val alhamdulillah: String = "الحمد لله"
+        val allahu_akbar: String = "الله أكبر"
+        val khatema : String = " لا إله إلا الله وحده لا شريك له، له الملك، وله الحمد، وهو على كل شيء قدير"
+    }
+}
--- app/src/main/java/com/route/islamic37/main_activity/main_fragments/TasbeehFragment.kt
+++ app/src/main/java/com/route/islamic37/main_activity/main_fragments/TasbeehFragment.kt
@@ -4,10 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.route.islamic37.Constants
 import com.route.islamic37.R
 
+
 class TasbeehFragment : Fragment() {
+
+    lateinit var image : ImageButton
+    lateinit var counterXml : TextView
+    lateinit var phase : TextView
+     var counter : Int = 0
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,4 +27,66 @@ class TasbeehFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_tasbeeh, container, false)
     }
-}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        image = view.findViewById(R.id.sebha_img)
+        counterXml = view.findViewById(R.id.counter)
+        counterXml.text = "$counter"
+        phase = view.findViewById(R.id.phase)
+        image.setOnClickListener { onClickTasbehLogic() }
+
+    }
+
+    fun onClickTasbehLogic(){
+
+        counter++
+        counterXml.text = "$counter"
+
+        if (phase.text == Constants.khatema){
+
+            phase.text = Constants.subhan_allah
+            counter = 0
+            counterXml.text = "$counter"
+
+
+            Toast.makeText(activity?.applicationContext, "لقد انهيت التسبيح", Toast.LENGTH_LONG).show()
+
+
+        }
+
+
+        if ( counter == 33){
+            if(phase.text == Constants.subhan_allah) {
+
+                phase.text = Constants.alhamdulillah
+                counter = 0
+                counterXml.text = "$counter"
+                }
+
+
+            else if (phase.text == Constants.alhamdulillah){
+                phase.text = Constants.allahu_akbar
+                counter = 0
+                counterXml.text = "$counter"
+
+                }
+            else if (phase.text == Constants.allahu_akbar){
+                phase.text = Constants.khatema
+                counter = 0
+                counterXml.text = "$counter"
+
+
+
+            }
+
+        }
+
+
+        }
+
+
+
+
+    }
