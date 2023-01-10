e74172375b370c7c7950fb749d5901672e0ac3eb
Shady Gamal <Shady_ql@yahoo.com>
2023-01-10 17:25:16 +0200

    Added ahadeth feature

--- app/src/main/java/com/route/islamic37/Constants.kt
+++ app/src/main/java/com/route/islamic37/Constants.kt
@@ -1,6 +1,16 @@
 package com.route.islamic37
 
-object Constants {
-    val EXTRA_SURA_POSITION = "SuraPosition"
-    val EXTRA_SURA_NAME_KEY = "SuraName"
+class Constants {
+
+    companion object {
+
+        val subhan_allah: String = "سبحان الله"
+        val alhamdulillah: String = "الحمد لله"
+        val allahu_akbar: String = "الله أكبر"
+        val khatema : String = " لا إله إلا الله وحده لا شريك له، له الملك، وله الحمد، وهو على كل شيء قدير"
+        val content_intent: String = "content_intent"
+        val position_intent : String = "position_intent"
+        val EXTRA_SURA_POSITION = "SuraPosition"
+        val EXTRA_SURA_NAME_KEY = "SuraName"
+    }
 }
